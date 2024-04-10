beb3c2e09a5c19fc01841c3201b97d2dec2177ce
Med Chakkir <demahomali01@gmail.com>
2024-04-10 17:46:19 +0300

    inital commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+// Modal
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelector(".btn--show-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+
+const openModal = e => {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = e => {
+  e.preventDefault();
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
